f6d240fe8038523148dea2334d12d5095e24c0c9
unknown <rhiki.080bct033@tcioe.edu.np>
2026-01-23 07:18:34 +0545

    Updated Schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    date_of_registration TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    username VARCHAR(100) NOT NULL UNIQUE
+);
