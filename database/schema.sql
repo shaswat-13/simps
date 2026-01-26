8ca0b598b78659ab63204d62ecbf239107991524
unknown <rhiki.080bct033@tcioe.edu.np>
2026-01-26 12:02:54 +0545

    Minor changes

--- database/schema.sql
+++ database/schema.sql
@@ -5,6 +5,5 @@ CREATE TABLE IF NOT EXISTS users (
     password_hash VARCHAR(255) NOT NULL,
     date_of_registration TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     username VARCHAR(100) NOT NULL UNIQUE,
-    is_verified BOOLEAN DEFAULT FALSE
 );
 
